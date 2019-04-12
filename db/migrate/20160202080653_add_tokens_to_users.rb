913d58847fa7752343ff7403e7f82dc4c83fea37
Christoph Petschnig <christoph.petschnig@sage.com>
2019-04-12 08:41:44 +0200

    Update migrations

--- db/migrate/20160202080653_add_tokens_to_users.rb
+++ db/migrate/20160202080653_add_tokens_to_users.rb
@@ -1,4 +1,4 @@
-class AddTokensToUsers < ActiveRecord::Migration
+class AddTokensToUsers < ActiveRecord::Migration[4.2]
   def change
     add_column :users, :access_token, :string
     add_column :users, :refresh_token, :string
--- db/migrate/20160901124425_add_api_country_code_to_user.rb
+++ db/migrate/20160901124425_add_api_country_code_to_user.rb
@@ -1,4 +1,4 @@
-class AddApiCountryCodeToUser < ActiveRecord::Migration
+class AddApiCountryCodeToUser < ActiveRecord::Migration[4.2]
   def change
     add_column :users, :api_country_code, :string
   end
